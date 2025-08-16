fc8f54a4cbc14cd786bab32b9c1b9e83816040b0
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:33 +0530

    Initial commit: Expense-Tracker

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Expense-Tracker"</h1>
+        <p>"Financial management app"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
